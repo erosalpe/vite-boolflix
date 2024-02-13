613d04471516566f5d4bf9e03565c77f4c6cab78
erosalpe <eros.alpegiani@gmail.com>
2024-02-13 16:26:51 +0100

    feat(FilmCard) aggiunte info al clic della card

--- src/components/Main/FilmCard.vue
+++ src/components/Main/FilmCard.vue
@@ -21,7 +21,36 @@
 <template>
   
     <div id="filmCard">
-        <img :src="'https://image.tmdb.org/t/p/w342'+ propsElement.poster_path" :alt="propsElement.poster_path">
+        <img :src="'https://image.tmdb.org/t/p/w342'+ propsElement.poster_path" :alt="propsElement.poster_path" v-if="propsElement.info === false">
+        <div id="mediaInfo" class="d-flex flex-column text-light gap-4 p-3" v-else>
+            <span>
+                <span class="fw-bold">
+                    Titolo:
+                </span> 
+                {{ propsElement.title }}
+            </span>
+            <span>
+                <span class="text-light fw-bold">
+                    Titolo originale:
+                </span> 
+                {{ propsElement.original_title }}
+            </span>
+            <span>
+                <span class="text-light fw-bold">
+                    Voto:
+                </span>
+                {{ propsElement.vote_average }}
+            </span>
+            <div class="d-flex flex-column overflow-y-auto">
+                <span class="text-light fw-bold mb-2">
+                    Overview:
+                </span>
+                <span id="overview" class="overflow-y-auto">
+                    {{ propsElement.overview }}
+                </span>
+            </div>
+            
+        </div>
     </div>
 
   
@@ -45,6 +74,10 @@
         img{
             width: 100%;
             height: 100%;
+        }
+        #mediaInfo{
+            width: 100%;
+            height: 503.69px;
         }
     }
 
