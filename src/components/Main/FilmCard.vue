733a325370fcbfb77662db5ef2bb9c78f34e8ef5
erosalpe <eros.alpegiani@gmail.com>
2024-04-14 17:27:23 +0200

    fix(FilmCard) cambiati id con classi

--- src/components/Main/FilmCard.vue
+++ src/components/Main/FilmCard.vue
@@ -26,55 +26,55 @@
 
 <template>
   
-    <div id="filmCard" class="rounded">
-        <img :src="'https://image.tmdb.org/t/p/w342'+ propsElement.poster_path" :alt="propsElement.poster_path" v-if="propsElement.info === false" class="rounded">
-        <div id="mediaInfo" class="d-flex flex-column text-light gap-4 p-3" v-else>
-            <span>
-                <span class="fw-bold">
-                    Titolo:
-                </span> 
-                {{ propsElement.title }}
-            </span>
-            <span>
-                <span class="text-light fw-bold">
-                    Titolo originale:
-                </span> 
-                {{ propsElement.original_title }}
-            </span>
-            <span>
-                <span class="fw-bold">
-                    Lingua originale:
-                </span> 
-                <img class="languageFlag"
-                    :src="'https://flagcdn.com/16x12/'+ propsElement.original_language +'.png'"
-                    :alt="propsElement.original_language"
-                >
-            </span>
-            <span>
-                <span class="text-light fw-bold">
-                    Attori nel film:
-                </span> 
-                <br>
-                <a :href="'https://it.wikipedia.org/wiki/'+ wikName(propsElement.cast[index].name)" target=”_blank” v-for="index in 4" class="link-underline link-underline-opacity-0">{{propsElement.cast[index].name}} &nbsp; </a>
-            </span>
-            <span>
-                <span class="text-light fw-bold">
-                    Voto:
+    <div class="rounded filmCard">
+            <img :src="'https://image.tmdb.org/t/p/w342'+ propsElement.poster_path" :alt="propsElement.poster_path" v-if="propsElement.info === false" class="rounded">
+            <div class="d-flex flex-column text-light gap-4 p-3 mediaInfo" v-else>
+                <span>
+                    <span class="fw-bold">
+                        Titolo:
+                    </span> 
+                    {{ propsElement.title }}
                 </span>
-                <span v-for="index in propsElement.vote_average">
-                    &#11088;
+                <span>
+                    <span class="text-light fw-bold">
+                        Titolo originale:
+                    </span> 
+                    {{ propsElement.original_title }}
                 </span>
-            </span>
-            <div class="d-flex flex-column overflow-y-auto">
-                <span class="text-light fw-bold mb-2">
-                    Overview:
+                <span>
+                    <span class="fw-bold">
+                        Lingua originale:
+                    </span> 
+                    <img class="languageFlag"
+                        :src="'https://flagcdn.com/16x12/'+ propsElement.original_language +'.png'"
+                        :alt="propsElement.original_language"
+                    >
                 </span>
-                <span id="overview" class="overflow-y-auto">
-                    {{ propsElement.overview }}
+                <span>
+                    <span class="text-light fw-bold">
+                        Attori nel film:
+                    </span> 
+                    <br>
+                    <a :href="'https://it.wikipedia.org/wiki/'+ wikName(propsElement.cast[index].name)" target=”_blank” v-for="index in 4" class="link-underline link-underline-opacity-0">{{propsElement.cast[index].name}} &nbsp; </a>
                 </span>
+                <span>
+                    <span class="text-light fw-bold">
+                        Voto:
+                    </span>
+                    <span v-for="index in propsElement.vote_average">
+                        &#11088;
+                    </span>
+                </span>
+                <div class="d-flex flex-column overflow-y-auto">
+                    <span class="text-light fw-bold mb-2">
+                        Overview:
+                    </span>
+                    <span id="overview" class="overflow-y-auto">
+                        {{ propsElement.overview }}
+                    </span>
+                </div>
+                
             </div>
-            
-        </div>
     </div>
 
   
@@ -90,7 +90,10 @@
 
 
 <style lang="scss" scoped>
-    #filmCard{
+    .h-0{
+        height: 0px;
+    }
+    .filmCard{
         background: rgb(68,68,68);
         background: radial-gradient(circle, rgba(68,68,68,1) 0%, rgba(0,0,0,1) 100%);
         --gap: 2rem;
@@ -100,7 +103,7 @@
             width: 100%;
             height: 100%;
         }
-        #mediaInfo{
+        .mediaInfo{
             width: 100%;
             height: 503.69px;
             .languageFlag{
@@ -141,7 +144,6 @@
             var(--sb-track-color);
         }
     }
-
     
 
 
